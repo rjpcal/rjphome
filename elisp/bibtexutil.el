e0ddb7f251f1788629e84019151bc8e8646c8712
Robert J. Peters <rjpcal@gmail.com>
2002-08-04 22:05:35 +0000

    Initial revision

---
+++ elisp/bibtexutil.el
@@ -0,0 +1,56 @@
+;;; $Id$
+
+(defvar bibtex-uniqid-re "uniqid[ \t\n]*=[ \t\n]*\\([01-9]+\\),")
+
+(defvar bibtex-last-uniqid 0)
+
+(defun bibtex-find-last-uniqid ()
+  (interactive)
+  (save-excursion
+	 (widen)
+	 (beginning-of-buffer)
+	 (setq max-id 0)
+	 (while (re-search-forward bibtex-uniqid-re (point-max) t)
+		(if (> (string-to-int (match-string 1)) max-id)
+			 (setq max-id (string-to-int (match-string 1)))
+		  ()))
+	 (setq bibtex-last-uniqid max-id)
+	 (message "last uniqid is %d" bibtex-last-uniqid)))
+
+(defun bibtex-add-uniqid ()
+  (interactive)
+  (bibtex-beginning-of-entry)
+  (forward-line 1)
+  (bibtex-find-last-uniqid)
+  (setq bibtex-last-uniqid (+ bibtex-last-uniqid 1))
+  (insert "  uniqid =       " (int-to-string bibtex-last-uniqid) ",\n"))
+
+(defun bibtex-find-uniqid (id)
+  (interactive "sfind uniqid #: ")
+  (beginning-of-buffer)
+  (re-search-forward (concat "uniqid[ \t\n]*=[ \t\n]*" id ","))
+  (bibtex-beginning-of-entry))
+
+(defun bibtex-recase ()
+  (interactive)
+  (save-excursion
+     (bibtex-find-text t)
+	  (setq beg (point))
+	  (bibtex-find-text nil)
+	  (setq end (point))
+	  (downcase-region beg end)
+	  (upcase-initials-region beg end)
+	) 
+)
+
+(defun my-bibtex-mode-hook ()
+  (local-set-key "\C-c\C-u" 'bibtex-add-uniqid)
+  (local-set-key "\C-c\C-f" 'bibtex-find-uniqid)
+  (local-set-key "\C-c\C-a" 'bibtex-recase))
+
+(setq bibtex-mode-hook 'my-bibtex-mode-hook)
+
+(provide 'bibtexutil)
+
+;; bibtexutil.el ends here
+
